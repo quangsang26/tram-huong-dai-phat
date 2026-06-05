9565b2bb8cc1b226dfe6242e39a2be652ed096ff
quangsang26 <qs26k5@gmail.com>
2026-06-05 12:22:31 +0700

    Use Render API endpoint

--- tram-huong-dai-phat/frontend/src/services/api.js
+++ tram-huong-dai-phat/frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://tram-huong-dai-phat.onrender.com",
+  baseURL: "https://tram-huong-dai-phat.onrender.com/api",
 });
 
 api.interceptors.request.use((config) => {
